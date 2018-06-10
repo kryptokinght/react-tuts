027c98ce98d7c219ab11f84a54b485bf7e089d36
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-10 22:34:14 +0530

    onSelectVideo event handler added

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -3,7 +3,10 @@ import VideoListItem from '../components/video_list_item';
 
 const VideoList = (props) => {
 	const videos = props.videos.map(video => {
-		return <VideoListItem key={video.etag} video={video} />
+		return <VideoListItem
+			onVideoSelect= {props.onVideoSelect}
+			key={video.etag} video={video}
+			/>
 	});
 
 	return (
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -12,9 +12,15 @@ const API_KEY = 'AIzaSyBapyhL7mIy-nT64WAiXf0uTdHxCsWg_mM';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {videos: []};
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 		YTSearch({key: API_KEY, term: "Apple"}, videos => {
-			this.setState({videos})
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			})
 		});
 	}
 
@@ -22,8 +28,11 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect= {selectedVideo => {this.setState( {selectedVideo} )} }
+					videos= {this.state.videos}
+				/>
 			</div>
 		);
 	}
