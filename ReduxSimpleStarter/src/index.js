8943d0f5cb1f20ea880886d02ae5f521d413978d
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-10 09:05:16 +0530

    SearchBar component first try

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,9 +1,18 @@
+import SearchBar from './components/search_bar';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyBapyhL7mIy-nT64WAiXf0uTdHxCsWg_mM';
+
+
 //Create a new component
 //This will produce some new HTML
 const App = () => {
-	return <div>Hi</div>; //JSX
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 //take this component and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
