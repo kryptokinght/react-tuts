275c46d52746dacd4edd8ac83cedf43b3ea87db8
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-10 08:19:21 +0530

    first working component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 //Create a new component
 //This will produce some new HTML
-const App = function() {
+const App = () => {
 	return <div>Hi</div>; //JSX
 }
 //take this component and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
