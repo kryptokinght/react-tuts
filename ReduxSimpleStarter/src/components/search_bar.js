ef40f87258397f0d2379bb01bec276fbec5da7cb
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-10 09:31:33 +0530

    state change searchBar first attempt

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-	render() {
-		return <input />;
+	constructor(props) {
+		super(props);
+		this.state = {term: ""};
 	}
+
+	render() {
+		return (
+			<div>
+				<input onChange={event => this.setState({term: event.target.value})} />
+			</div>
+		);
+	}
+
 }
 
 export default SearchBar;
