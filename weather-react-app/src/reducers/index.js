e07976adc2f5b0e63c7b4fb4223ba73bb5993496
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 13:44:45 +0530

    add weather-reducer to index of reducers

--- weather-react-app/src/reducers/index.js
+++ weather-react-app/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import WeatherReducer from './reducer_weather';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weatherData: WeatherReducer
 });
 
 export default rootReducer;
