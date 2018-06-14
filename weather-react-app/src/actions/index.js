5e19aca90e8eeff9cec7789d27b2aa667723c00c
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 08:51:52 +0530

    create action fetchWeather

---
+++ weather-react-app/src/actions/index.js
@@ -0,0 +1,16 @@
+import API_KEY from '../../api_keys/keys';
+import axios from 'axios';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city}, IN`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
