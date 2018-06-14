0d2eb2511057942fbf9b40493e0c0a1d352b3f64
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 14:50:03 +0530

    Chart component finished and integrated into WeatherList

--- weather-react-app/src/containers/weather-list.js
+++ weather-react-app/src/containers/weather-list.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
 	renderWeather(cityData) {
 		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressure = cityData.list.map(weather => weather.main.pressure);
+		const humidity = cityData.list.map(weather => weather.main.humidity);
 		return (
 			<tr key = { name }>
 				<td>{ name }</td>
+				<td>
+					<Chart data={temps} color="orange" unit="K" />
+				</td>
+				<td>
+					<Chart data={pressure} color="black" unit="hPa" />
+				</td>
+				<td>
+					<Chart data={humidity} color="blue" unit="%" />
+				</td>
 			</tr>
 		);
 	}
@@ -20,9 +33,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
