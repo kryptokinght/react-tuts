b54b8b6ff2c2eeda519eddabb83683bcb692fb64
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 13:39:58 +0530

    2 bugs corrected

--- weather-react-app/src/reducers/reducer_weather.js
+++ weather-react-app/src/reducers/reducer_weather.js
@@ -1,11 +1,11 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
 //appends each city weather data to state
-export default function(state = null, action) {
-	switch(actio.type) {
+export default function(state = [], action) {
+	switch(action.type) {
 	case FETCH_WEATHER:
 		//appending city weather data to state
-		return [ action.payload.data, ...state ]; //state.concat([action.payload.data])
+		return state.concat([action.payload.data]); //state.concat([action.payload.data])
 	}
 	return state;
 }
