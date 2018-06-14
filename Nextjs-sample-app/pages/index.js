09ac154be0aa1576e5277431ba3798fafc7b1c5d
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 16:56:54 +0530

    create basic routes for about and home

--- Nextjs-sample-app/pages/index.js
+++ Nextjs-sample-app/pages/index.js
@@ -1,1 +1,15 @@
-export default () => <div>Welcome to next.js!</div>
+import Link from 'next/link';
+
+const Index = () => {
+	return (
+		<div>
+			<ul>
+				<li><Link href="/"><a>Home</a></Link></li>
+				<li><Link href="/about"><a>About</a></Link></li>
+			</ul>
+			<h1>Home Page</h1>
+		</div>
+	);
+}
+
+export default Index;
