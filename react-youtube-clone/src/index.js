e7beffb56c11636f98266887d2005f564b4923a0
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-14 07:49:35 +0530

    added API_KEY to another hidden folder

--- react-youtube-clone/src/index.js
+++ react-youtube-clone/src/index.js
@@ -5,8 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-
-const API_KEY = 'AIzaSyBapyhL7mIy-nT64WAiXf0uTdHxCsWg_mM';
+import API_KEY from '../api_keys/keys';
 
 //Create a new component
 //This will produce some new HTML
