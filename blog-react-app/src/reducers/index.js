746c6d21ba7aaaaa5d176c7b3b718f931f0c494f
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-17 23:37:43 +0530

    (added) reducer_posts for action FETCH_POSTS

--- blog-react-app/src/reducers/index.js
+++ blog-react-app/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from 'reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: postsReducer
 });
 
 export default rootReducer;
