560c155cd5a08f423e2eb430ec454f6d224ab963
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-18 00:18:03 +0530

    (refractored) improve variable naming

--- blog-react-app/src/index.js
+++ blog-react-app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import promise from 'redux-promise';
+import ReduxPromise from 'redux-promise';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 
 ReactDOM.render(
