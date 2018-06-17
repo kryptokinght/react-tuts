5b8cebe02e6561255c005fb36f9e6ee4ef85caca
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-17 21:15:11 +0530

    (created) basic example of react-router-dom

--- blog-react-app/src/index.js
+++ blog-react-app/src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+    render() {
+        return <div>Hello! Its me!</div>
+    }
+}
+
+class Goodbye extends React.Component {
+    render() {
+        return <div>Goodbye! Its me!</div>
+    }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+            <Route path="/hello" component={Hello} />
+            <Route path="/goodbye" component={Goodbye} />
+        </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
