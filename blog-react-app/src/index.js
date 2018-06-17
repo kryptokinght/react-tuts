75b5df0815995cd1b09c0b95eb8cba50f6b37316
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-17 23:18:22 +0530

    (created) our first action fetchPosts()

--- blog-react-app/src/index.js
+++ blog-react-app/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
 
 import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 
 ReactDOM.render(
