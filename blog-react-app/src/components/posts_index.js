70a8c2001640fa690f224f095c9d8a88eb72d45b
kryptokinhgt <minanshu.1998@gmail.com>
2018-06-17 22:43:20 +0530

    (created) first Router definition

---
+++ blog-react-app/src/components/posts_index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+	render() {
+		return <div>Posts Index!</div>
+	}
+}
+
+export default PostsIndex;
--- blog-react-app/src/index.js
+++ blog-react-app/src/index.js
@@ -4,30 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
+import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-    render() {
-        return <div>Hello! Its me!</div>
-    }
-}
-
-class Goodbye extends React.Component {
-    render() {
-        return <div>Goodbye! Its me!</div>
-    }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
-            <h1>My static header</h1>
-            <Route path="/hello" component={Hello} />
-            <Route path="/goodbye" component={Goodbye} />
+            <Route path="/" component={PostsIndex} />
         </div>
     </BrowserRouter>
   </Provider>
