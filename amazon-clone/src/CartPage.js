0188c9874496df7f1196c23130bc9e68d0436b0a
Minanshu Singh <minanshu.1998@gmail.com>
2018-12-04 00:02:41 +0530

    add empty cart msg

--- amazon-clone/src/CartPage.js
+++ amazon-clone/src/CartPage.js
@@ -5,34 +5,34 @@ import "./CartPage.css";
 
 const CartPage = ({ items, onRemoveOne, onAddOne }) => {
   let total = 0;
-  return (
-    <ul className="CartPage-items">
-      {items.map(item => {
-        total = total + item.price * item.count;
-        return (
-          <li key={item.id} className="CartPage-item">
-            <Item item={item}>
-              <div className="CartItem-controls">
-                <button
-                  className="CartItem-removeOne"
-                  onClick={() => onRemoveOne(item)}
-                >
-                  &ndash;
-                </button>
-                <span className="CartItem-count">{item.count}</span>
-                <button
-                  className="CartItem-addOne"
-                  onClick={() => onAddOne(item)}
-                >
-                  +
-                </button>
-              </div>
-            </Item>
-          </li>
-        );
-      })}
-      <span className="Cart-total">Total: ${total}</span>
-    </ul>
+  if(items.length === 0)
+    return (
+        <div className='Cart-empty'>
+            <p>Your cart is empty</p>
+            <p>Want to add some expensive products to it?</p>
+        </div>
+    );
+  else 
+    return (
+        <ul className="CartPage-items">
+        {items.map(item => {
+            total = total + item.price * item.count;
+            return (
+            <li key={item.id} className="CartPage-item">
+                <Item item={item}>
+                <div className="CartItem-controls">
+                    <button className="CartItem-removeOne" onClick={() => onRemoveOne(item)}>
+                    &ndash;</button>
+                    <span className="CartItem-count">{item.count}</span>
+                    <button className="CartItem-addOne" onClick={() => onAddOne(item)}>
+                    +</button>
+                </div>
+                </Item>
+            </li>
+            );
+        })}
+        <span className="Cart-total">Total: ${total}</span>
+        </ul>
   );
 };
 
